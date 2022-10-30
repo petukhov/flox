63bb5d19968a781844c667931847f76a5d2cf6e4
petukhov <petukhovg@gmail.com>
2022-10-30 14:27:42 +0800

    added recursive function to handle interactive input

--- Program.fs
+++ Program.fs
@@ -2,15 +2,28 @@
 
 // For more information see https://aka.ms/fsharp-console-apps
 
+let readLines (filePath:string) = seq {
+    use sr = new IO.StreamReader (filePath)
+    while not sr.EndOfStream do
+        yield sr.ReadLine ()
+}
+
 let args = Environment.GetCommandLineArgs()
 
-let runFile arg =
-    printfn "Welcome to runFile %s" arg
+let runFile pathStr =
+    printfn "Reading file: %s" pathStr
+    let allText = readLines pathStr |> String.concat "\n"
+    printfn "%s" allText
 
 let runPrompt () =
     printfn "Welcome to runPrompt"
-    let input = Console.ReadLine()
-    printfn "Your input was: %s" input
+    let rec processInput input =
+        match input with
+        | "" -> ()
+        | _ -> 
+            printfn "Your input was: %s" input
+            processInput (Console.ReadLine())
+    processInput (Console.ReadLine())
 
 
 if args.Length > 2 then
