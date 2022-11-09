3cb2bb20e1b3b5cc57d179c3f7be9ef455e044b0
petukhov <petukhovg@gmail.com>
2022-11-09 20:43:20 +0800

    added pretty printing for expressions

--- Expr.fs
+++ Expr.fs
@@ -9,25 +9,35 @@ open flox.Lexing
 // "Literal  : Object value",
 // "Unary    : Token operator, Expr right"
 
-// Should be automatically converted to this:
+
+// Should be converted to this:
 
 type Expr =
     | Binary of left : Expr * operator : Token * right : Expr
     | Grouping of expression : Expr
-    | Literal of value : obj
+    | Literal of value : obj option
     | Unary of operator: Token * right : Expr
 
 
 let internal createToken tokenType text literal line =
-    {
-        tokenType = tokenType;
-        lexeme = text;
-        literal = literal;
-        line = line
-    }
+    {   tokenType = tokenType
+        lexeme = text
+        literal = literal
+        line = line   }
 
-let internal example = 
+let example = 
     Binary(
-        Unary(createToken MINUS "-" None 1, Literal(123)), 
-        createToken STAR "*" None 1, Grouping(Literal(45.67))
+        Unary(createToken MINUS "-" None 1, Literal(Some(123 :> obj))),
+        createToken STAR "*" None 1,
+        Grouping(Literal(Some(45.67 :> obj)))
     )
+
+let rec prettyPrint = function
+    | Binary(left, operator, right) -> parenthesize operator.lexeme [left; right]
+    | Grouping(expression) -> parenthesize "group" [expression]
+    | Literal(value) -> match value with Some(v) -> string v | None -> "nil"
+    | Unary(operator, right) -> parenthesize operator.lexeme [right]
+
+and parenthesize name exprs =
+    let paramsStr = exprs |> List.map prettyPrint |> String.concat " "
+    $"({name} {paramsStr})"
